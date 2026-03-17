ebf23e22823ceb611ae499dc76cdcced1f95bf2c
SRVHCCC <chaurasiyahimanshu2002@gmail.com>
2026-03-17 13:56:01 +0530

    change in config file

--- src/Common/config/config.js
+++ src/Common/config/config.js
@@ -1,4 +1,4 @@
 const API_URL =
-  import.meta.env.VITE_MODE === "local" ? "http://localhost:5000/api" : "";
+  import.meta.env.VITE_MODE === "local" ? "http://localhost:5000/api" : "https://incubation-manegment-backend.onrender.com/api";
 
 export default API_URL;
